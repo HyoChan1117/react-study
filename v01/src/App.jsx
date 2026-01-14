a9ae828dc96137d2698a040d1125cbf932fbc393
HyoChan Kim <skkhc0317@gmail.com>
2026-01-14 14:59:48 +0000

    26.01.14 Update to do list react

--- v01/src/App.jsx
+++ v01/src/App.jsx
@@ -1,7 +1,5 @@
-import ToDoList from "./components/to-do-list";
+import ToDoList from "./components/to-do-list-v02";
 
 export default function MyApp() {
-  return (
-    <ToDoList />
-  )
+  return <ToDoList />;
 }
